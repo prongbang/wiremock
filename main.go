b52390b1654d1bb35a680e49800e60da8c80be6a
anonymous <anonymous>
2020-06-22 14:30:31 +0700

    get port from env

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"github.com/prongbang/wiremock/cmd"
 	"github.com/prongbang/wiremock/pkg/config"
@@ -10,8 +11,12 @@ import (
 func main() {
 	port := flag.String("port", "8000", "a string port")
 	flag.Parse()
+	p := *port
+	if os.Getenv("PORT") != "" {
+		p = os.Getenv("PORT")
+	}
 	conf := config.Config{
-		Port: *port,
+		Port: p,
 	}
 	cmd.Run(conf)
 }
